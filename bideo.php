f800a4c86ee51494608208b486be1afa2def0493
JosueCab <71083497+JosueCab@users.noreply.github.com>
2022-10-25 11:57:51 +0200

    Add files via upload
    video-js

---
+++ bideo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+  <title>Bideoak javascript</title>
+  <link href="video-js.min.css" rel="stylesheet">
+  <script src="video.min.js"></script>
+</head>
+
+<body>
+  <div></div>
+  <video class="video-js vjs-default-skin" width="320" height="240" controls data-setup='{}'>
+    <source src="bideoak/mondongo.mp4" type="video/mp4">
+  </video>
+</body>
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+
+<h1>
+
+<?php echo '<p>Hola mundo</p>'; ?>
+
+</h1>
+<h2><a href="bideo.php">Bideoak ikusi VoD</a></h2>
+<button type = "button" href = "streaming.html"> Streaming </button>
+
+</body>
+
+
+</html>
